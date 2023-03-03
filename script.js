196424e638a2e3b7b7aa4e4e8c236ae2ad615cce
Valeriya <valeriyalukovkina@gmail.com>
2023-03-03 20:52:15 +0200

    add adaptive

---
+++ script.js
@@ -0,0 +1,10 @@
+;(function openMenu(selectorTop) {
+    let header = document.querySelector(selectorTop);
+    let btn = header.querySelector('.nav_btn');
+    let nav = header.querySelector('.nav');
+    btn.addEventListener('click', function() {
+        btn.classList.toggle('nav_btn_close');
+        btn.classList.toggle('nav_btn_open');
+        nav.classList.toggle('nav_visible')
+    })
+})('.header');
